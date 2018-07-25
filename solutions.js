3e5eaa445b98cdc2272c1f6dd86997b5b073a938
DanielJBailey <Dbailey0210@gmail.com>
2018-07-25 10:34:45 -0500

    finish #1, basic algorithms

---
+++ solutions.js
@@ -0,0 +1,8 @@
+/// BASIC ALGORITHM SCRIPTING
+///___________________________________________________
+/// 1.  Convert Celsius to Fahrenheit
+function convertToF(celsius) {
+    let fahrenheit = celsius * (9/5) + 32;
+    return fahrenheit;
+}
+
